b6a9b78dbd8c3de1eee2d1b9f9e7eeb9ee898cbc
landq123 <842528209@qq.com>
2020-03-13 16:45:16 +0800

    add:新增header-line工具插件

--- src/components/menuConfig/menu.js
+++ src/components/menuConfig/menu.js
@@ -1,104 +1,107 @@
- const tipsMenu = [
-     {
-         router: '/debounce',
-         title: '防抖',
-     },
-     {
-         router: '/throttle',
-         title: '节流',
-     },
-     {
-         router: '/dfs',
-         title: '深度优先、广度优先遍历',
-     },
-     {
-         router: '/rewrite-apply',
-         title: '手写apply',
-     },
-     {
-         router: '/flatArr',
-         title: '数组扁平化',
-     },
-     {
-         router: '/axios',
-         title: 'axios测试',
-     },
-     {
-         router: '/axios-blob',
-         title: 'axios处理文件流',
-     },
-     {
-         router: '/arr-concat',
-         title: '数组排序',
-     },
-     {
-         router: '/css-tips',
-         title: 'css小技巧',
-     },
-     {
-         router: '/reset-message',
-         title: '重写message',
-     },
-     {
-         router: '/extend',
-         title: 'extend写message',
-     },
-     {
-         router: '/authorized',
-         title: '权限渲染',
-     },
-     {
-         router: '/keepAlive',
-         title: '缓存和滚动',
-     },
-     {
-         router: '/directive',
-         title: '自定义指令',
-     },
-     {
-         router: '/arry',
-         title: '数组相关方法',
-     },
- ];
- const toolsMenu = [{
-         router: '/js-zip',
-         title: '下载压缩包'
-     },
+const tipsMenu = [{
+        router: '/debounce',
+        title: '防抖',
+    },
+    {
+        router: '/throttle',
+        title: '节流',
+    },
+    {
+        router: '/dfs',
+        title: '深度优先、广度优先遍历',
+    },
+    {
+        router: '/rewrite-apply',
+        title: '手写apply',
+    },
+    {
+        router: '/flatArr',
+        title: '数组扁平化',
+    },
+    {
+        router: '/axios',
+        title: 'axios测试',
+    },
+    {
+        router: '/axios-blob',
+        title: 'axios处理文件流',
+    },
+    {
+        router: '/arr-concat',
+        title: '数组排序',
+    },
+    {
+        router: '/css-tips',
+        title: 'css小技巧',
+    },
+    {
+        router: '/reset-message',
+        title: '重写message',
+    },
+    {
+        router: '/extend',
+        title: 'extend写message',
+    },
+    {
+        router: '/authorized',
+        title: '权限渲染',
+    },
+    {
+        router: '/keepAlive',
+        title: '缓存和滚动',
+    },
+    {
+        router: '/directive',
+        title: '自定义指令',
+    },
+    {
+        router: '/arry',
+        title: '数组相关方法',
+    },
+];
+const toolsMenu = [{
+        router: '/js-zip',
+        title: '下载压缩包',
+    },
 
-     {
-         router: '/jspdf',
-         title: '生成pdf'
-     },
-     {
-         router: '/clipboard',
-         title: '复制粘贴'
-     },
-     {
-         router: '/autosize',
-         title: '高度自适应文字'
-     },
-     {
-         router: '/progressbar',
-         title: '进度条'
-     },
-     {
-         router: '/qriously',
-         title: '二维码生成'
-     },
-     {
-         router: '/excel',
-         title: 'excel相关'
-     },
-     {
-         router: '/sortable',
-         title: '多选框拖拽排序'
-     },
-     {
-         router: '/vue-draggable-resizable',
-         title: '拖拽'
-     },
- ]
- export const menus = {
-     tipsMenu,
-     toolsMenu
- };
+    {
+        router: '/jspdf',
+        title: '生成pdf',
+    },
+    {
+        router: '/clipboard',
+        title: '复制粘贴',
+    },
+    {
+        router: '/autosize',
+        title: '高度自适应文字',
+    },
+    {
+        router: '/progressbar',
+        title: '进度条',
+    },
+    {
+        router: '/qriously',
+        title: '二维码生成',
+    },
+    {
+        router: '/excel',
+        title: 'excel相关',
+    },
+    {
+        router: '/sortable',
+        title: '多选框拖拽排序',
+    },
+    {
+        router: '/vue-draggable-resizable',
+        title: '拖拽',
+    },
+    {
+        router: '/leader-line',
+        title: 'js划线库',
+    },
+];
+export const menus = {
+    tipsMenu,
+    toolsMenu,
+};
---
+++ src/views/leader-line.vue
@@ -0,0 +1,60 @@
+<!--  -->
+<template>
+  <div class="container">
+    <span id="span1">444</span>
+    <span id="span2">555</span>
+  </div>
+</template>
+
+<script>
+import LeaderLine from '@/plugin/leader-line';
+export default {
+	data() {
+		return {};
+	},
+
+	components: {},
+
+	computed: {},
+
+	mounted() {
+		this.lineInit();
+	},
+
+	methods: {
+		lineInit() {
+			let start = document.getElementById('span1');
+			let end = document.getElementById('span2');
+
+			new LeaderLine(start, LeaderLine.pointAnchor(end, { x: 0, y: '45%' }), {
+				dash: { len: 14, gap: 4, animation: true },
+				color: '#2d79ec',
+				startPlug: 'arrow3',
+				endPlug: 'arrow3',
+				startPlugSize: 4,
+				endPlugSize: 4,
+				path: 'grid', // 折线
+				startSocket: 'right',
+				endSocket: 'left',
+				size: 2,
+			});
+		},
+	},
+};
+</script>
+<style lang='scss' scoped>
+.container {
+	width: 500px;
+	height: 500px;
+	display: flex;
+	justify-content: space-between;
+	span {
+		padding: 10px;
+		background: red;
+		color: #fff;
+	}
+	#span1 {
+		height: 100px;
+	}
+}
+</style>
