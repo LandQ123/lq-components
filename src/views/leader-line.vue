05c6a8efde058673b04aebbbec1b2182ec4947dd
landq123 <842528209@qq.com>
2020-03-19 09:33:53 +0800

    feat：完善jszip

--- src/views/leader-line.vue
+++ src/views/leader-line.vue
@@ -1,6 +1,7 @@
 <!--  -->
 <template>
   <div class="container">
+    <a href="https://anseki.github.io/leader-line/">https://anseki.github.io/leader-line/</a>
     <span id="span1">444</span>
     <span id="span2">555</span>
   </div>
@@ -10,7 +11,9 @@
 import LeaderLine from '@/plugin/leader-line';
 export default {
 	data() {
-		return {};
+		return {
+			line: null,
+		};
 	},
 
 	components: {},
@@ -26,25 +29,32 @@ export default {
 			let start = document.getElementById('span1');
 			let end = document.getElementById('span2');
 
-			new LeaderLine(start, LeaderLine.pointAnchor(end, { x: 0, y: '45%' }), {
-				dash: { len: 14, gap: 4, animation: true },
-				color: '#2d79ec',
-				startPlug: 'arrow3',
-				endPlug: 'arrow3',
-				startPlugSize: 4,
-				endPlugSize: 4,
-				path: 'grid', // 折线
-				startSocket: 'right',
-				endSocket: 'left',
-				size: 2,
-			});
+			this.line = new LeaderLine(
+				start,
+				LeaderLine.pointAnchor(end, { x: 0, y: '45%' }),
+				{
+					dash: { len: 14, gap: 4, animation: true },
+					color: '#2d79ec',
+					startPlug: 'arrow3',
+					endPlug: 'arrow3',
+					startPlugSize: 4,
+					endPlugSize: 4,
+					path: 'grid', // 折线
+					startSocket: 'right',
+					endSocket: 'left',
+					size: 2,
+				},
+			);
 		},
+	},
+	beforeDestroy() {
+		this.line.remove();
 	},
 };
 </script>
 <style lang='scss' scoped>
 .container {
-	width: 500px;
+	width: 700px;
 	height: 500px;
 	display: flex;
 	justify-content: space-between;
